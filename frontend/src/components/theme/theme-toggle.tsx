809d6ad831d5d2c0a08aceac9ea5e4cee3230200
bernardogazola <bernardogazola@pm.me>
2026-04-20 18:07:22 -0300

    Frontend: theme toggle add

---
+++ frontend/src/components/theme/theme-toggle.tsx
@@ -0,0 +1,22 @@
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+const ToggleTheme = () => {
+	const { resolvedTheme, setTheme } = useTheme();
+
+	return (
+		<Button
+			variant="ghost"
+			size="icon"
+			className="cursor-pointer"
+			onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+		>
+			<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-300 dark:-rotate-90 dark:scale-0" />
+			<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-300 dark:rotate-0 dark:scale-100" />
+			<span className="sr-only">Alternar tema</span>
+		</Button>
+	);
+};
+
+export default ToggleTheme;
