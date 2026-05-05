2b0288e5b1940001e1053ce5f5cc2c5f32711726
bernardogazola <bernardogazola@pm.me>
2026-05-05 13:30:24 -0300

    Backend (domain): setId User

--- backend/agendafacil-domain/src/main/java/br/pucpr/agendafacil/domain/identity/User.java
+++ backend/agendafacil-domain/src/main/java/br/pucpr/agendafacil/domain/identity/User.java
@@ -44,6 +44,10 @@ public abstract class User {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
